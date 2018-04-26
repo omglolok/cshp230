4ff756ad32112f10873bc034e5347d027ee5bfcd
medmonds <medmonds@homeaway.com>
2018-04-26 01:37:40 -0700

    commit Homework 01

---
+++ BirthdayCard/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Models.Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("CardSent", message);
+            }
+            return View();
+        }
+    }
+}
---
+++ BirthdayCard/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BirthdayCard.Models
+{
+    public class Message
+    {
+        [Required(ErrorMessage="Please enter From")]
+        public string From { get; set; }
+        [Required(ErrorMessage="Please enter To")]
+        public string To { get; set; }
+        [Required(ErrorMessage="Please enter a birthday message")]
+        public string BirthdayMessage { get; set; }
+    }
+}
